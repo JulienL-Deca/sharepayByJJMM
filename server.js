b376d38eb4c9d78fdffaff0ff3c468282f99a9a0
JULIEN LI <31632821+JulienL-Deca@users.noreply.github.com>
2018-05-15 15:49:47 +0200

    add error management and public folder

--- server.js
+++ server.js
@@ -23,6 +23,18 @@ app.get("/", function (request, result) {
 
 
 
+//public ressource
+app.use(express.static("public"));
+
+//errors management
+app.use(function (req, res, next) {
+  res.status(404).render("template_error", {catchedError: "404 NOT FOUND"});
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('ERROR 500 : Something broken!')
+});
 
 //server test
 
