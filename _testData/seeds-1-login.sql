87ad2e02d9a9b88afb0a0b9387a0b1d8855a2e6f
JULIEN LI <31632821+JulienL-Deca@users.noreply.github.com>
2018-05-15 17:11:01 +0200

    add a folder for test data

---
+++ _testData/seeds-1-login.sql
@@ -0,0 +1,10 @@
+--
+-- Data for Name: userlogin; Type: TABLE DATA; Schema: public; Owner: Martin
+--
+
+COPY public.userlogin (id, last_name, first_name, email, password) FROM stdin;
+00f41ad3-1f65-4650-b122-b3c0b272632d	LITTLER	Julien	julien.littler@decathlon.com	1234
+6fdf8002-7709-4211-9357-a05eb79312a2	DELORME	Martin	martin.delorme@decathlon.com	1234
+f8ad19d3-0992-4a61-b87d-536c9245fba1	DEBACK	Matthieu	matthieu.deback@decathlon.com	1234
+b59695ff-7398-4d0b-bcdf-3b4c640f1231	LI	Jiao	jiao.li@decathlon.com	1234
+\.
---
+++ _testData/seeds-4-expenses.sql
@@ -0,0 +1,9 @@
+--
+-- Data for Name: expenses; Type: TABLE DATA; Schema: public; Owner: Martin
+--
+
+COPY public.expenses (id, date, amount, description, paid_by, event_id) FROM stdin;
+c2323369-4dc3-49e8-9e15-5288b705b102	2018-05-15	10040	Squash	f23648bb-f19c-40fc-a9c5-7595463c1728	354e3ffb-da5b-41e8-a1cc-5d14ab91c4fd
+4a895f5c-d376-4d75-9d1c-46865a44924a	2018-05-15	3000	Bières pour tous	f01c9ec2-1fa9-4090-aab4-e1ca2934f94b	354e3ffb-da5b-41e8-a1cc-5d14ab91c4fd
+636dd9d3-a96a-42a0-94b5-07283a9f8091	2018-05-15	1800	2ème tournée	0c02449d-6e19-4857-86a2-1444c4ea8bbc	354e3ffb-da5b-41e8-a1cc-5d14ab91c4fd
+\.
