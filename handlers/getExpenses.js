9d376455a99fc9ac74b4a553dcb1f1edb075f16b
Martin D <martin.delorme@decathlon.com>
2018-05-17 13:16:37 +0200

    updates

--- handlers/getExpenses.js
+++ handlers/getExpenses.js
@@ -2,11 +2,12 @@ const PG = require("pg");
 
 function getExpenses(request, result) {
   const client = new PG.Client(process.env.DATABASE_URL);
+  console.log(request);
   client.connect();
 
   client.query(
-    "SELECT expenses.description, expenses.amount, participants.nickname, expenses.date, events.name FROM events LEFT JOIN expenses ON (expenses.event_id = events.id) LEFT JOIN participants ON (participants.id = expenses.paid_by) WHERE events.id=$1::uuid",
-    [request.params.id])
+    "SELECT expenses.description, expenses.amount, participants.nickname, expenses.date, events.name FROM events LEFT JOIN expenses ON (expenses.event_id = events.id) LEFT JOIN participants ON (participants.id = expenses.paid_by) LEFT JOIN userlogin ON (participants.user_id = userlogin.id) WHERE events.id=$1::uuid AND userlogin.id=$2::uuid",
+    [request.params.id, request.user.id])
     .catch((error) => {
       console.warn(error)
       client.end();
@@ -14,10 +15,10 @@ function getExpenses(request, result) {
     .then((rawsOnly) => rawsOnly.rows)
     .then((response) => {
       result.render("expenses", {expenses: response})
-      //console.log(JSON.stringify(response[0]));
+      console.log(JSON.stringify(response));
       client.end();
     })
-  
+
   }
 
   module.exports = getExpenses;
