242d62d158f3a602fbe0db067e79e7c77682d3c6
Martin D <martin.delorme@decathlon.com>
2018-05-17 11:50:01 +0200

    new changes in back and front

--- handlers/getExpenses.js
+++ handlers/getExpenses.js
@@ -5,7 +5,7 @@ function getExpenses(request, result) {
   client.connect();
 
   client.query(
-    "SELECT description, amount, participants.nickname, date, events.name FROM events LEFT JOIN expenses ON (expenses.event_id = events.id) LEFT JOIN participants ON (participants.id = expenses.paid_by) WHERE events.id=$1::uuid",
+    "SELECT expenses.description, expenses.amount, participants.nickname, expenses.date, events.name FROM events LEFT JOIN expenses ON (expenses.event_id = events.id) LEFT JOIN participants ON (participants.id = expenses.paid_by) WHERE events.id=$1::uuid",
     [request.params.id])
     .catch((error) => {
       console.warn(error)
@@ -14,22 +14,10 @@ function getExpenses(request, result) {
     .then((rawsOnly) => rawsOnly.rows)
     .then((response) => {
       result.render("expenses", {expenses: response})
-      console.log(JSON.stringify(response[0]));
+      //console.log(JSON.stringify(response[0]));
       client.end();
     })
-    // client.query(
-    // "SELECT name FROM events WHERE events.id=$1::uuid",
-    // [request.params.id])
-    // .catch((error) => {
-    //   console.warn(error)
-    //   client.end();
-    // })
-    // .then((rawsOnly) => rawsOnly.rows)
-    // .then((resp) => {
-    //   result.render("expenses", {event: resp[0].name})
-    //         console.log(JSON.stringify(resp[0]));
-    //   client.end();
-    // })
+  
   }
 
   module.exports = getExpenses;
