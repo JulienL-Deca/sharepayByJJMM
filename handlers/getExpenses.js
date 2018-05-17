2be02ff0e6941481f3eab2d466e9fd2098cd831b
Matthieu Deback <matthieu.deback@decathlon.com>
2018-05-17 18:00:55 +0200

    new layout and modif expense

--- handlers/getExpenses.js
+++ handlers/getExpenses.js
@@ -36,5 +36,20 @@ const client = new PG.Client(process.env.DATABASE_URL);
 }
 
 
+  client.query(
+    "SELECT expenses.description, expenses.amount, participants.nickname, expenses.date, events.name FROM events LEFT JOIN expenses ON (expenses.event_id = events.id) LEFT JOIN participants ON (participants.id = expenses.paid_by) LEFT JOIN userlogin ON (participants.user_id = userlogin.id) WHERE events.id=$1::uuid AND userlogin.id=$2::uuid",
+    [request.params.id, request.user.id])
+    .catch((error) => {
+      console.warn(error)
+      client.end();
+    })
+    .then((rawsOnly) => rawsOnly.rows)
+    .then((response) => {
+      result.render("expenses", {expenses: response, user: request.user.first_name})
+      console.log(JSON.stringify(response));
+      client.end();
+    })
+
+  }
 
   module.exports = getExpenses;
