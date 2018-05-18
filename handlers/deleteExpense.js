1d4511fdcb5a4a04f110aea2f1017b5bf22a1027
Matthieu Deback <matthieu.deback@decathlon.com>
2018-05-18 11:13:33 +0200

    modif delete expense1

--- handlers/deleteExpense.js
+++ handlers/deleteExpense.js
@@ -4,5 +4,13 @@ function deleteExpense(request, result){
 
 const client = new PG.Client({connectionString: process.env.DATABASE_URL});
 client.connect();
-
 }
+
+
+
+
+
+
+
+
+module.exports= deleteExpense
